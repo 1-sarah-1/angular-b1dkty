d0bad0e8538b6e614e5a7ba8e98ae1d7892f7205
1-sarah-1 <sarah.pfaeffli@sbb.ch>
2022-06-02 10:16:33 +0200

    working version of preselection of multiselection

--- src/app/select-option-groups-multi-selection-example.ts
+++ src/app/select-option-groups-multi-selection-example.ts
@@ -10,80 +10,61 @@ import { FormControl, FormBuilder, FormGroup } from '@angular/forms';
   templateUrl: 'select-option-groups-multi-selection-example.html',
 })
 export class SelectOptionGroupsMultiSelectionExample {
-  prjMgrCtrlTech = new FormControl();
+  form: FormGroup;
+  myForm: FormGroup;
   entriesOfGroups: EntriesOfGroup[] = [];
 
-  form: FormGroup;
-  foodFromTheWorld: any[] = [
-    {
-      nation: 'Italy',
-      food: [
-        { value: 'lasagna', viewValue: 'Lasagna' },
-        { value: 'parmigiana', viewValue: 'Parmigiana' },
-        { value: 'pasta', viewValue: 'Pasta' },
-      ],
-    },
-    {
-      nation: 'Germany',
-      food: [
-        { value: 'weißwurst', viewValue: 'Weißwurst' },
-        { value: 'eisbein', viewValue: 'Eisbein' },
-        { value: 'rheinische-muscheln', viewValue: 'Rheinische Muscheln' },
-      ],
-    },
-    {
-      nation: 'Switzerland',
-      food: [
-        { value: 'birchermüesli', viewValue: 'Birchermüesli' },
-        { value: 'rösti', viewValue: 'Rösti' },
-        { value: 'bündnernusstorte', viewValue: 'Bündnernusstorte' },
-      ],
-    },
-  ];
-
   constructor(formBuilder: FormBuilder) {
-    this.form = formBuilder.group({
-      value: [[]],
-    });
+    //this.form = formBuilder.group({
+    //      value: [[]],
+    //});
 
     const group1Entry1 = {
       display: 'Tiramisu',
       tooltip: 'Tiramisu Tooltip',
-    }
+    };
 
     const group1Entry2 = {
       display: 'Eis',
       tooltip: 'Eis Tooltip',
-    }
+    };
 
     const entriesOfGroup1 = {
       group: 'Dessert',
-      entries: [group1Entry1, group1Entry2]
+      entries: [group1Entry1, group1Entry2],
     };
 
     const group2Entry1 = {
       display: 'Salat',
       tooltip: 'Salat Tooltip',
-    }
+    };
 
     const group2Entry2 = {
       display: 'Suppe',
       tooltip: 'Suppe Tooltip',
-    }
+    };
 
     const entriesOfGroup2 = {
       group: 'Vorspeise',
-      entries: [group2Entry1, group2Entry2]
+      entries: [group2Entry1, group2Entry2],
     };
 
     this.entriesOfGroups.push(entriesOfGroup1);
     this.entriesOfGroups.push(entriesOfGroup2);
+
+    this.form = formBuilder.group({
+      value: [['Blau']],
+    });
+
+    this.myForm = formBuilder.group({
+      value: [[group2Entry1.display, group1Entry2.display]],
+    });
   }
 }
 
 function isPreselected(value: string): boolean {
- // if (value === 'Vorspeise') {
-   // return true;
+  // if (value === 'Vorspeise') {
+  // return true;
   //}
   return true;
 }
